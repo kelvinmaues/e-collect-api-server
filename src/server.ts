dea7fc7b0b5d6dd0e5005f2d33f9285baf1cbf37
Kelvin Maues <kgmdeveloper@gmail.com>
2020-06-02 22:43:54 -0300

    Testing HTTP requests

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,30 @@ import express from "express";
 
 const app = express();
 
+const users = ["Karina", "Ruth", "Ivone", "Ícaro"];
+
 app.get("/users", (req, res) => {
+  const { search } = req.query;
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(String(search)))
+    : users;
   console.log("Listagem de usuários");
-  res.json(["Karina", "Ruth", "Ivone", "Ícaro"]);
+  res.json(filteredUsers);
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users[Number(id)];
+  return res.json(user);
+});
+
+app.post("/users", (req, res) => {
+  const user = {
+    name: "Kelvin",
+    email: "kgmdeveloper@gmail.com",
+  };
+  return res.json(user);
 });
 
 app.listen(3333);
