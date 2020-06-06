f7cc8575e1644e07342f0e397610e434a228468a
Kelvin Maues <kgmdeveloper@gmail.com>
2020-06-06 11:50:04 -0300

    Add main routes to index

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
+// routes
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+// Set routes
+app.use(routes);
 
 app.listen(3333);
