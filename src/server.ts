6ac5d1377dcd50f5c3e97f94c48d7208bfbf72c2
Kelvin Maues <kgmdeveloper@gmail.com>
2020-06-06 11:40:02 -0300

    Cleaning the project

--- src/server.ts
+++ src/server.ts
@@ -2,30 +2,10 @@ import express from "express";
 
 const app = express();
 
-const users = ["Karina", "Ruth", "Ivone", "Ícaro"];
+app.use(express.json());
 
-app.get("/users", (req, res) => {
-  const { search } = req.query;
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(String(search)))
-    : users;
-  console.log("Listagem de usuários");
-  res.json(filteredUsers);
-});
-
-app.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const user = users[Number(id)];
-  return res.json(user);
-});
-
-app.post("/users", (req, res) => {
-  const user = {
-    name: "Kelvin",
-    email: "kgmdeveloper@gmail.com",
-  };
-  return res.json(user);
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello World" });
 });
 
 app.listen(3333);
